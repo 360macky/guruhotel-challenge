9de67470fc96936f687905205ed51fe9d64c06bd
Marcelo Arias <hello@marceloarias.com>
2022-12-15 18:30:40 -0500

    Add YelpCDN to Next configuration file

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.yelpcdn.com',
+        port: '',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
