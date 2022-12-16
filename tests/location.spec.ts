4109a560d8060f2d763937f20330e7ac56cb277d
Marcelo Arias <hello@marceloarias.com>
2022-12-16 08:16:37 -0500

    Remove colon from comments in location.spec.ts

--- tests/location.spec.ts
+++ tests/location.spec.ts
@@ -3,12 +3,12 @@ import { test, expect } from '@playwright/test'
 test('Check the user flow for the location test', async ({ page }) => {
   await page.goto('https://guruhotel-yelp-app.vercel.app/')
 
-  // Check that there is a button with the text "Allow location access":
+  // Check that there is a button with the text "Allow location access"
   await expect(
     page.locator('button:text("Allow location access")')
   ).toBeVisible()
 
-  // After click the button check that the browsers asks for location access:
+  // After click the button check that the browsers asks for location access
   await page.click('button:text("Allow location access")')
 
   // Grant location access to the browser:
