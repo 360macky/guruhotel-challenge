1bc39d286d96e84af011545757b3ba663f7eb3f4
Marcelo Arias <hello@marceloarias.com>
2022-12-15 20:03:37 -0500

    Format Redux store

--- store/store.ts
+++ store/store.ts
@@ -1,46 +1,47 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { createSlice } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit'
+import { Business } from '../pages/types/Business'
 
 export interface appState {
-  results: any[];
-  resultsSeen: string[];
-  lastSearch: string;
-  latestResults: any;
-  currentLocation: string;
+  results: Business[]
+  resultsSeen: string[]
+  lastSearch: string
+  latestResults: Business[]
+  currentLocation: string
 }
 
 export const appSlice = createSlice({
-  name: "app",
+  name: 'app',
   initialState: {
     results: [],
     resultsSeen: [],
-    lastSearch: "",
+    lastSearch: '',
     latestResults: [],
-    currentLocation: "",
+    currentLocation: '',
   },
   reducers: {
     resultsUpdate: (state: appState, action) => {
-      state.results = action.payload;
+      state.results = action.payload
     },
     resultsSeenAdd: (state: appState, action) => {
-      state.resultsSeen.push(action.payload);
+      state.resultsSeen.push(action.payload)
     },
-    lastSearchUpdate:  (state: appState, action) => {
-      state.lastSearch = action.payload;
+    lastSearchUpdate: (state: appState, action) => {
+      state.lastSearch = action.payload
     },
-    latestResultsUpdate:  (state: appState, action) => {
-      state.latestResults = action.payload;
+    latestResultsUpdate: (state: appState, action) => {
+      state.latestResults = action.payload
     },
-    currentLocationUpdate:  (state: appState, action) => {
-      state.currentLocation = action.payload;
-    }
+    currentLocationUpdate: (state: appState, action) => {
+      state.currentLocation = action.payload
+    },
   },
-});
+})
 
 const store = configureStore({
   reducer: appSlice.reducer,
-});
+})
 
-export default store;
+export default store
 
-export const appAction = appSlice.actions;
+export const appAction = appSlice.actions
