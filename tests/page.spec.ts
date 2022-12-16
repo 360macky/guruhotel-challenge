658e7ad469b55c4ba25b511c6f0463cc536e9ca9
Marcelo Arias <hello@marceloarias.com>
2022-12-15 22:11:23 -0500

    Add Playwright tests

---
+++ tests/page.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('Check basic elements of the index page', async ({ page }) => {
+  await page.goto('https://yelp-next-app.vercel.app/');
+
+  // Expect a title to contain the text "Yelp Next App - GuruHotel Challenge"
+  await expect(page).toHaveTitle('Yelp Next App - GuruHotel Challenge');
+
+  // Expect the navbar to contain Yelp Next App
+  await expect(page.locator('text="Yelp Next App"')).toBeVisible();
+
+  // Expect the page to have a button element with the text "Search"
+  await expect(page.locator('button:text("Search")')).toBeVisible();
+
+});
